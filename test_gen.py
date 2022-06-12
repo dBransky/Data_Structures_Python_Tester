b22e53f1eb876dcf12da829eef05ba109da49464
Daniel <d.s.bransky@gmail.com>
2022-06-12 23:51:31 +0300

    fixed init failed

--- test_gen.py
+++ test_gen.py
@@ -11,8 +11,8 @@ class TestGen:
                       ('SumOfBumpGradeBetweenTopWorkersByGroup', self.rand_test_sum),
                       ('AcquireCompany', self.rand_test_acq),
                       ('RemoveEmployee', self.rand_test_rem),
-                      ('CompanyValue', self.rand_test_val)
-                      # ('BumpGradeToEmployees', self.rand_test_bump)
+                      ('CompanyValue', self.rand_test_val),
+                      ('BumpGradeToEmployees', self.rand_test_bump)
                       ]
 
         self.companies = random.randint(0, 50)
@@ -25,20 +25,20 @@ class TestGen:
             file.write('Quit')
 
     def rand_test_bump(self):
-        return f'BumpGradeToEmployees  {str(random.randint(-1, 1100000000))} {str(random.randint(1000000000, 2100000000))} {str(random.randint(-1, 2100000000))}'
+        return f'BumpGradeToEmployees  {str(random.randint(-1, 1100000000))} {str(random.randint(1000000000, 2100000000))} {str(random.randint(-1, 2100000))}'
 
     def rand_test_add(self):
         self.total_emp += 1
-        return f'AddEmployee {str(random.randint(-1, 10000))} {str(random.randint(-1, self.companies + 1))}  {str(random.randint(-1, 2100000000))}'
+        return f'AddEmployee {str(random.randint(-1, 10000))} {str(random.randint(-1, self.companies + 1))}  {str(random.randint(-1, 2100000))}'
 
     def rand_test_sal_increase(self):
-        return f'EmployeeSalaryIncrease {str(random.randint(-1, 10000))} {str(random.randint(-1, 2100000000))}'
+        return f'EmployeeSalaryIncrease {str(random.randint(-1, 10000))} {str(random.randint(-1, 2100000))}'
 
     def rand_test_promote(self):
-        return f'PromoteEmployee {str(random.randint(-1, 10000))} {str(random.randint(-1, 2100000000))}'
+        return f'PromoteEmployee {str(random.randint(-1, 10000))} {str(random.randint(-1, 2100000))}'
 
     def rand_test_avg(self):
-        return f'AverageBumpGradeBetweenSalaryByGroup  {str(random.randint(-1, self.companies + 1))} {str(random.randint(-1, 1100000000))} {str(random.randint(1000000000, 2100000000))}'
+        return f'AverageBumpGradeBetweenSalaryByGroup  {str(random.randint(-1, self.companies + 1))} {str(random.randint(-1, 1000000))} {str(random.randint(1000000, 2100000))}'
 
     def rand_test_sum(self):
         return f'SumOfBumpGradeBetweenTopWorkersByGroup  {str(random.randint(-1, self.companies + 1))} {str(random.randint(-1, int((self.total_emp + 1)/((self.companies)+1))))}'
